11ba24e7adfd82d34616b341e9b380eb67db29d3
LittleFinchy <smfinch99@gmail.com>
2021-06-04 00:00:06 -0400

    put war client class into new file
    war client runner is in its own file

--- lib/war_client_runner.rb
+++ lib/war_client_runner.rb
@@ -1,28 +1,4 @@
-require "socket"
-
-class WarClient
-  attr_reader :socket
-  attr_reader :output
-
-  def initialize(port)
-    @socket = TCPSocket.new("localhost", port)
-  end
-
-  def provide_input(text)
-    @socket.puts(text)
-  end
-
-  def capture_output(delay = 0.1)
-    sleep(delay)
-    @output = @socket.read_nonblock(1000).chomp # not gets which blocks
-  rescue IO::WaitReadable
-    @output = ""
-  end
-
-  def close
-    @socket.close if @socket
-  end
-end
+require_relative "war_client"
 
 client = WarClient.new(3335)
 while true
